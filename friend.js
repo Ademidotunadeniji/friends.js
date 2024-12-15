686b609e1e610e5b5d4a2435b17d759b77b87474
Ademidotunadeniji <ademidotunadeniji32@gmail.com>
2024-12-15 10:48:41 +0100

    first commit

---
+++ friend.js
@@ -0,0 +1,9 @@
+// let friends = ['Bola', 'Adeola', 'Femi', 'Tobi', 'Henry'];
+
+// console.log(friends)
+
+const friends = ['Bola', 'Adeola', 'Femi', 'Tobi', 'Henry'];
+
+const bestfriend = friends [0]; // choose Bola as my bestfriend (index 1)
+
+console.log(bestfriend);
